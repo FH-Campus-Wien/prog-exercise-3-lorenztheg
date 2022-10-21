194b2ac6f71f75fe46d065bda4e557d5a869b086
lorenztheg <lorenz571lola@gmail.com>
2022-10-21 10:53:39 +0200

    First Layout

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -4,10 +4,60 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class App {
+    public void oneMonthCalender(){
 
-    // Implement all methods as public static
+    }
+
+    public void lcg(){
+
+    }
+
+
+    public void guessingGame(){
+
+    }
+    public int randomNumberBetweenOneAndHundred(){
+        return 0; // Random class verwenden
+
+    }
+
+
+    public void swapArrays(){
+
+    }
+
+    public void camelCase(){
+
+    }
+
+    public void checkDigit(){
+
+    }
+
 
     public static void main(String[] args) {
+        App exercise3 = new App();
+
+        System.out.println("Task 1: One Month Calendar");
+        exercise3.oneMonthCalender();
+
+        System.out.println("Task 2: Pseudo Random Numbers");
+        exercise3.lcg();
+
+        System.out.println("Task 3: Guessing Game");
+        exercise3.guessingGame();
+
+        System.out.println("Task 4: Swap Arrays");
+        exercise3.swapArrays();
+
+        System.out.println("Task 5: Camel Case");
+        exercise3.camelCase();
+
+        System.out.println("Task 6: Check Digit");
+        exercise3.checkDigit();
+
+
+
         // test your method implementations here
         // make method calls
         // print their results
